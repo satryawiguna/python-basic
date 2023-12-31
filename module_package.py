8660c9e6831d4349a20d15e99520b5c0337464a9
Satrya Wiguna <satrya@freshcms.net>
2023-12-31 16:03:19 +0800

    Lecture 12: Module and Packages

---
+++ module_package.py
@@ -0,0 +1,5 @@
+# import packages.my_module as my_module
+from packages.my_module import *
+
+# my_module.func_in_module()
+func_in_module()
---
+++ packages/my_module.py
@@ -0,0 +1,2 @@
+def func_in_module():
+    print("I'm a Super User ever in the world")
